6efccc0626d6e6976a0ae1e6e9da16b1424a2986
Dave <davidrsharples@gmail.com>
2013-11-12 18:33:52 -0500

    Initial Commit

---
+++ FirstImpressions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstImpressions
+//
+//  Created by David Sharples on 11/12/13.
+//  Copyright (c) 2013 SamAlexDave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DRSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DRSAppDelegate class]));
+	}
+}
