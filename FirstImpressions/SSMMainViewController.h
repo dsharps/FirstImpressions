dca5020cea43de86378e7ba30b2c5b662ca679e3
samanthamerritt <merritts@seas.upenn.edu>
2013-11-12 19:12:31 -0500

    Added a storyboard, and edited plist file

---
+++ FirstImpressions/SSMMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSMMainViewController.h
+//  FirstImpressions
+//
+//  Created by Samantha Merritt on 11/12/13.
+//  Copyright (c) 2013 SamAlexDave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSMMainViewController : UIViewController
+
+@end
---
+++ FirstImpressions/SSMMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  SSMMainViewController.m
+//  FirstImpressions
+//
+//  Created by Samantha Merritt on 11/12/13.
+//  Copyright (c) 2013 SamAlexDave. All rights reserved.
+//
+
+#import "SSMMainViewController.h"
+
+@interface SSMMainViewController ()
+
+@end
+
+@implementation SSMMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
