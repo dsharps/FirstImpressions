feafed31c848cb4da740ad522ff9f9904e9e1a0f
samanthamerritt <merritts@seas.upenn.edu>
2013-11-12 21:42:48 -0500

    Set up the parse backend

--- FirstImpressions/SSMMainViewController.m
+++ FirstImpressions/SSMMainViewController.m
@@ -7,12 +7,63 @@
 //
 
 #import "SSMMainViewController.h"
+#import <Parse/Parse.h>
 
 @interface SSMMainViewController ()
+
+@property (nonatomic, strong) IBOutlet UITextView *inputMessage;
+@property (nonatomic, strong) IBOutlet UILabel *outputMessage;
+
+
+-(IBAction)sendAMessageToParse:(id)sender;
+-(IBAction)retrieveAMessageFromParse:(id)sender;
 
 @end
 
 @implementation SSMMainViewController
+
+-(IBAction)sendAMessageToParse:(id)sender{
+    PFQuery *query = [PFQuery queryWithClassName:@"Message"];
+    [query whereKey:@"messageName" equalTo:@"Test Message"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if(!error){
+            if([objects count] == 0){
+            //save message for first time
+                PFObject *parseMessage = [PFObject objectWithClassName:@"Message"];
+                parseMessage[@"messageName"] = @"Test Message";
+                parseMessage[@"body"] = _inputMessage.text;
+                [parseMessage saveInBackground];
+            } else {
+            //update message
+                PFObject *messageToUpdate = objects[0];
+                messageToUpdate[@"body"] = _inputMessage.text;
+                [messageToUpdate saveInBackground];
+            }
+        } else {
+            //error
+        }
+    }];
+}
+
+-(IBAction)retrieveAMessageFromParse:(id)sender{
+    PFQuery *query = [PFQuery queryWithClassName:@"Message"];
+    [query whereKey:@"messageName" equalTo:@"Test Message"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if(!error){
+            if([objects count] == 0){
+                //do nothing
+            } else {
+                //update message
+                PFObject *messageToShow = objects[0];
+                _outputMessage.text = [NSString stringWithFormat:@"%@",messageToShow[@"body"]];
+                
+                NSLog([NSString stringWithFormat:@"%@",messageToShow[@"body"]]);
+            }
+        } else {
+            //error
+        }
+    }];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
