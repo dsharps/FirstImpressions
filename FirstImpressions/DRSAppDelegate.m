91f2b8ed0687a73f3d367b8dcbd584a9d7816296
samanthamerritt <merritts@seas.upenn.edu>
2013-11-12 19:45:29 -0500

    Set up Parse!

--- FirstImpressions/DRSAppDelegate.m
+++ FirstImpressions/DRSAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "DRSAppDelegate.h"
+#import <Parse/Parse.h>
 
 @implementation DRSAppDelegate
 
@@ -16,10 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+    [Parse setApplicationId:@"Y7seXmEGWI7umFdEign2YEaZznaUw2impbRbSDVo"
+                  clientKey:@"R0WflgEI5ck96b4abx1kdU07SwhhWrCGtYQ6Hd3c"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
