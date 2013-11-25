8e3c2236c599cccfc10ae6ff861cb16226153e96
Alex Brashear <alexjbrashear@gmail.com>
2013-11-24 23:30:59 -0500

    just created files get back to it later

---
+++ FirstImpressions/SSMWouldYouRatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSMWouldYouRatherViewController.h
+//  FirstImpressions
+//
+//  Created by Alex Brashear on 11/24/13.
+//  Copyright (c) 2013 SamAlexDave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSMWouldYouRatherViewController : UIViewController
+
+@end
---
+++ FirstImpressions/SSMWouldYouRatherViewController.m
@@ -0,0 +1,38 @@
+//
+//  SSMWouldYouRatherViewController.m
+//  FirstImpressions
+//
+//  Created by Alex Brashear on 11/24/13.
+//  Copyright (c) 2013 SamAlexDave. All rights reserved.
+//
+
+#import "SSMWouldYouRatherViewController.h"
+
+@interface SSMWouldYouRatherViewController ()
+
+@end
+
+@implementation SSMWouldYouRatherViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
