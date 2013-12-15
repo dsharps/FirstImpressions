6c3bcf6ba1ec6d7c17509b46ec8e81a9eeaea5f0
Alex Brashear <alexjbrashear@gmail.com>
2013-12-15 12:35:19 -0500

    added some keyboard toolbars

--- FirstImpressions/SSMResponseViewController.m
+++ FirstImpressions/SSMResponseViewController.m
@@ -10,6 +10,9 @@
 
 @interface SSMResponseViewController ()
 
+@property (nonatomic, retain) UIToolbar *keyboardToolbar;
+
+
 @end
 
 @implementation SSMResponseViewController
@@ -66,12 +69,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setupKeyboardToolbar];
+    _inputText.inputAccessoryView = self.keyboardToolbar;
 	// Do any additional setup after loading the view.
 	NSLog([NSString stringWithFormat:@"MM Message Body: %@", self.messageManager.receivedMessage[@"body"]]);
 
 	//load the message text that we set from the previous view
 	_receivedMessageLabel.text = self.receivedMessage[@"body"];
 }
+///////////////////   keyboard toolbar stuff /////////////////////
+
+- (void)setupKeyboardToolbar
+{
+    if (self.keyboardToolbar == nil) {
+        self.keyboardToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 44)];
+        UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"done"
+                                                                       style:UIBarButtonItemStyleBordered
+                                                                      target:self
+                                                                      action:@selector(resignKeyboard:)];
+        UIBarButtonItem *extraspace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
+                                                                                    target:self
+                                                                                    action:nil];
+        [self.keyboardToolbar setItems:[[NSArray alloc] initWithObjects:extraspace, doneButton, nil]];
+    }
+}
+- (void)resignKeyboard:(id)sender
+{
+    [_inputText resignFirstResponder];
+}
+
+
+
+//////////////////////////////////////////////////////////////////
 
 - (void)viewWillAppear:(BOOL)animated
 {
